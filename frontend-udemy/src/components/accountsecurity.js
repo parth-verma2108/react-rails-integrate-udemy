0474de4db58191b750f5c9b3b56d51c2daf96f41
ongraphtech <parth.ongraph@gmail.com>
2022-10-02 16:19:12 +0530

    Create sidebar and modify account setting sub components

--- frontend-udemy/src/components/accountsecurity.js
+++ frontend-udemy/src/components/accountsecurity.js
@@ -3,15 +3,21 @@ import Edit from './images/edit.svg'
 import './css/accountsecurity.css'
 
 class accountsecurity extends Component {
+  isEditable = () => {
+    var element = document.getElementById('email')
+    if (element){
+      element.removeAttribute('disabled');
+    }
+  }
   render() {
     return (
       <div>
       <div>
         <form className="form-body" style={{textAlign: "left", padding:'0'}}>
           <div className="as-content">Email:</div>
-          <input className="input-field input-width ps-3" style={{width: '35rem'}} id="email" placeholder="email" type="email" value='p@gmail.com'/><img src={Edit}/><br />
+          <input className="input-field input-width ps-3" style={{width: '35rem'}} id="email" placeholder="email" type="email" value='p@gmail.com' disabled/><img src={Edit} onClick={this.isEditable}/><br />
           <div className="as-content">Password:</div>
-          <input className="input-field input-width ps-3" style={{width: '35rem'}} placeholder="password" type="password" value='*********' onChange={(e)=>{this.setState({password: e.target.value})}}/><img src={Edit}/><br />
+          <input className="input-field input-width ps-3" style={{width: '35rem'}} placeholder="password" type="password" value='*********' onChange={(e)=>{this.setState({password: e.target.value})}} disabled/><img src={Edit}/><br />
         </form>
       </div>
       <div style={{width:'35rem', border:'1px solid #dbdbdb', paddingLeft:'10px'}}>
